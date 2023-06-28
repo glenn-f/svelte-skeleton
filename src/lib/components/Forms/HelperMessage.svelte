c39a2a4cb5fc37003f6a0708328dbec4d0360f5a
Glenn Fonseca <glenn.fonseca@pmm.am.gov.br>
2023-06-28 16:45:17 -0400

    Feat: - novo componente HelperMessage - recomposição de Label com HelperMessage

---
+++ src/lib/components/Forms/HelperMessage.svelte
@@ -0,0 +1,70 @@
+<script>
+  /** Tipo da mensagem exibida. Altera o esquema de cores da mensagem.
+   * @type {"error" | "success" | "warning"} */
+  export let type = undefined
+
+  /** Mensagem para exibição no esquema de cores padrão.
+   * @type {string | string[]} */
+  export let message = undefined
+
+  /** Mensagem para exibição no esquema de cores de `erro`.
+   * @type {string | string[]} */
+  export let error = undefined
+
+  /** Mensagem para exibição no esquema de cores de `alerta`.
+   * @type {string | string[]} */
+  export let warning = undefined
+
+  /** Mensagem para exibição no esquema de cores de `sucesso`.
+   * @type {string | string[]} */
+  export let success = undefined
+
+  /** Reservar o espaço da mensagem quando ela estiver vazia (use quando estiver quebrando a formatação).
+   * Valor padrão: `false`
+   * @type {false | true} */
+  export let spaceHolding = false
+
+  /** Caso a mensagem seja um **Array**, este **separador** será utilizado na construção da mensagem final.
+   * Valor padrão: `', '`
+   * @type {string} */
+  export let sep = ', '
+
+  let _message = ''
+  let _type = ''
+
+  $: {
+    if (message?.length > 0) {
+      _message = Array.isArray(message) ? message.join(sep) : message
+      _type = type ?? ''
+    } else if (error?.length > 0) {
+      _message = Array.isArray(error) ? error.join(sep) : error
+      _type = 'error'
+    } else if (warning?.length > 0) {
+      _message = Array.isArray(warning) ? warning.join(sep) : warning
+      _type = 'warning'
+    } else if (success?.length > 0) {
+      _message = Array.isArray(success) ? success.join(sep) : success
+      _type = 'success'
+    } else {
+      _message = ''
+      _type = ''
+    }
+  }
+</script>
+
+<span
+  class="text-sm"
+  class:text-slate-500={_type == ''}
+  class:text-error-500-400-token={_type == 'error'}
+  class:text-warning-500-400-token={_type == 'warning'}
+  class:text-success-500-400-token={_type == 'success'}
+  class:spaceHolding
+>
+  {_message}
+</span>
+
+<style>
+  .spaceHolding:empty:before {
+    content: '\200b';
+  }
+</style>
--- src/lib/components/Forms/Label.svelte
+++ src/lib/components/Forms/Label.svelte
@@ -1,4 +1,6 @@
 <script>
+  import HelperMessage from './HelperMessage.svelte'
+
   export let labelClass = ''
   /** @type {?string} */
   export let label = ''
@@ -12,37 +14,18 @@
   export let success = undefined
   /** @type {true | false} Padrão `false` */
   export let errorSpacing = false
-  let errorMsg = ''
-  $: if (Array.isArray(error) && error.length > 0) {
-    errorMsg = error.join(', ')
-  } else if (typeof error == 'string' && error.length > 0) {
-    errorMsg = error
-  } else {
-    errorMsg = error ? error : ''
-  }
 </script>
 
 <label class={'label ' + labelClass}>
+  <!-- Label + Required Indicator (*) -->
   <span>
     {label}
     {#if label && (required === '' || Boolean(required))}
       <span class="text-red-400">&nbsp;*</span>
     {/if}
   </span>
+  <!-- Input -->
   <slot />
-  {#if errorMsg}
-    <span class="text-error-500-400-token text-sm">
-      {errorMsg}
-    </span>
-  {:else if warning && typeof warning == 'string'}
-    <span class="text-warning-500-400-token text-sm">
-      {warning}
-    </span>
-  {:else if success && typeof success == 'string'}
-    <span class="text-success-500-400-token text-sm">
-      {success}
-    </span>
-  {:else if errorSpacing}
-    <span>&nbsp;</span>
-  {/if}
+  <!-- Error / Warning / Success Message -->
+  <HelperMessage {error} {warning} {success} spaceHolding={errorSpacing} />
 </label>
