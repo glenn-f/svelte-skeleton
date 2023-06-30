215e8111275cf62a424476cccad8187c339e080e
Glenn Fonseca <glenn.fonseca@pmm.am.gov.br>
2023-06-30 11:48:26 -0400

    dev:
    - update debugger
    - reset db script

---
+++ src/lib/server/db/reset_schema.sql
@@ -0,0 +1,13 @@
+-- Active: 1687020775859@@127.0.0.1@3306
+
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS usuario_empresa;
+
+DROP TABLE IF EXISTS pessoa;
+
+DROP TABLE IF EXISTS sessao;
+
+DROP TABLE IF EXISTS empresa;
+
+DROP TABLE IF EXISTS usuario;
