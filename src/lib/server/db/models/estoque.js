e64cb2924789242f0f494645600f501b380d3558
Glenn Fonseca <glenn.fonseca@pmm.am.gov.br>
2023-07-14 16:20:15 -0400

    Feat: inicio de consulta a estoque

--- src/lib/server/db/models/estoque.js
+++ src/lib/server/db/models/estoque.js
@@ -1,7 +1,14 @@
 import { db, dbInsert, dbTransaction } from ".."
 
 export function consultarEstoques(dados) {
-
+  const { eid } = dados
+  try {
+    const query = db.prepare("SELECT e.*, p.nome p_nome, p.titulo_codigo, p.produto_categoria_id FROM estoque e JOIN produto p ON p.id = e.produto_id WHERE p.empresa_id = $eid")
+    const data = query.all({ eid })
+    return { valid: true, data }
+  } catch (e) {
+    return { valid: false, message: "Erro desconhecido", code: 'DB_UNKNOWN' }
+  }
 }
 
 export function detalharEstoque(dados) {
