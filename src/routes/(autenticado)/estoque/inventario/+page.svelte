c26e631c1825c4190a4ec38b6487b03fd67bdecf
Glenn Fonseca <glenn.fonseca@outlook.com>
2023-08-28 22:25:01 -0400

    feat: inventario por estado

--- src/routes/(autenticado)/estoque/inventario/+page.svelte
+++ src/routes/(autenticado)/estoque/inventario/+page.svelte
@@ -1,10 +1,13 @@
 <script>
+  import CardInventario from './CardInventario.svelte'
+
   import { Table } from '$lib/components/Table'
   import { renderComponent } from '@tanstack/svelte-table'
   import CelulaAcoes from './CelulaAcoes.svelte'
-  import { mapCondicao, mapEstadoEstoque, mapFluxoEstoque, mapOrigem } from '$lib/globals'
+  import { EE_AUDITORIA, EE_AVALIACAO, EE_DISPONIVEL, EE_MANUTENCAO, EE_USOINTERNO, mapCondicao, mapEstadoEstoque, mapFluxoEstoque, mapOrigem } from '$lib/globals'
   import { formatMoeda } from '$lib/helpers'
   import ShowChip from '$lib/components/ShowChip.svelte'
+  import Icon from '@iconify/svelte'
   export let data
 
   $: rows = data.estoques || []
@@ -22,11 +25,32 @@
     { header: 'Ações', cell: (info) => renderComponent(CelulaAcoes, { data: info.row.original }), enableSorting: false }
   ]
   const pageSizes = [10, 25, 50]
+  function contarEstados(estoques) {
+    if (!Array.isArray(estoques)) return null
+    const dados = {}
+    Array.from(mapEstadoEstoque).forEach(([id]) => (dados[id] = { qntd: 0, custo: 0, preco: 0 }))
+    for (let i = 0; i < estoques.length; i++) {
+      const e = estoques[i]
+      const tmp = dados[e.estado]
+      tmp.qntd += e.qntd || 0
+      tmp.custo += (e.qntd || 0) * e.custo || 0
+      tmp.preco += (e.qntd || 0) * e.preco_unitario || 0
+    }
+    return dados
+  }
+  $: inventarioDividido = contarEstados(rows)
 </script>
 
 <div class="grid gap-3">
   <div class="flex items-center">
     <h1 class="h1 text-center mr-3">Inventário</h1>
+  </div>
+  <div class="grid grid-cols-5 gap-2">
+    <CardInventario color="green" value={inventarioDividido?.[EE_DISPONIVEL]} title={mapEstadoEstoque.get(EE_DISPONIVEL)} icon="fa6-solid:check-double" />
+    <CardInventario color="yellow" value={inventarioDividido?.[EE_AVALIACAO]} title={mapEstadoEstoque.get(EE_AVALIACAO)} icon="mdi:tag-arrow-up" />
+    <CardInventario color="orange" value={inventarioDividido?.[EE_AUDITORIA]} title={mapEstadoEstoque.get(EE_AUDITORIA)} icon="fluent-mdl2:compliance-audit" />
+    <CardInventario color="blue" value={inventarioDividido?.[EE_MANUTENCAO]} title={mapEstadoEstoque.get(EE_MANUTENCAO)} icon="mdi:wrench-clock" />
+    <CardInventario color="gray" value={inventarioDividido?.[EE_USOINTERNO]} title={mapEstadoEstoque.get(EE_USOINTERNO)} icon="mdi:account-lock" />
   </div>
   <div class="grid gap-2">
     {#key data}
---
+++ src/routes/(autenticado)/estoque/inventario/CardInventario.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { formatInteger, formatMoeda } from '$lib/helpers'
+  import Icon from '@iconify/svelte'
+
+  export let icon, title, value, color
+  let { qntd, custo, preco } = value
+</script>
+
+<div class={`col-span-1 card p-2 card-hover !bg-${color}-500 border-2 border-${color}-700`}>
+  <span class="flex flex-col items-center gap-3">
+    <span class="flex justify-center items-center gap-2">
+      {#if icon}
+        <Icon {icon} width="36px" height="36px" />
+      {/if}
+      <span class="text-lg font-bold">
+        {title}
+      </span>
+    </span>
+    <div class="flex justify-between gap-2 text-md">
+      <div class="flex flex-col items-center">
+        <span class="font-bold">Quantidade</span>
+        <span>
+          {formatInteger(qntd) ?? '-'}
+        </span>
+      </div>
+      <div class="flex flex-col items-center">
+        <span class="font-bold"> Custo </span>
+        <span>
+          {formatMoeda(custo) ?? '-'}
+        </span>
+      </div>
+      <div class="flex flex-col items-center">
+        <span class="font-bold"> Preço </span>
+        <span>
+          {formatMoeda(preco) ?? '-'}
+        </span>
+      </div>
+    </div>
+  </span>
+</div>
