796c15ee32e2b4e40e6b76054b0a9aca8731c802
Glenn Fonseca <glenn.fonseca@outlook.com>
2023-09-04 01:13:00 -0400

    fix: sizing

--- src/lib/components/Table/RowStatusToggle.svelte
+++ src/lib/components/Table/RowStatusToggle.svelte
@@ -1,7 +1,9 @@
 <script>
   import { enhance } from '$app/forms'
   import { SlideToggle, toastStore } from '@skeletonlabs/skeleton'
-  export let checked, id, disabled = undefined
+  export let checked,
+    id,
+    disabled = undefined
   export let action = '?/alternarStatus',
     method = 'POST',
     name = 'status'
@@ -13,23 +15,28 @@
   }
 </script>
 
-<div class="flex flex-nowrap gap-3">
-  <form
-    bind:this={form}
-    {action}
-    {method}
-    use:enhance={(_) => {
-      return ({ result, update }) => {
-        if (result.data.form.valid) {
-          toastStore.trigger({ message: result.data.form.message, background: 'variant-filled-success' })
-        } else {
-          toastStore.trigger({ message: result.data.form.message, background: 'variant-filled-error' })
-        }
-        update()
+<form
+  bind:this={form}
+  {action}
+  {method}
+  use:enhance={(_) => {
+    return ({ result, update }) => {
+      if (result.data.form.valid) {
+        toastStore.trigger({ message: result.data.form.message, background: 'variant-filled-success' })
+      } else {
+        toastStore.trigger({ message: result.data.form.message, background: 'variant-filled-error' })
       }
-    }}
-  >
-    <input type="hidden" name="id" value={id} />
-    <SlideToggle {disabled} on:click={handleAlternar} {name} {checked} size="sm" active="bg-green-500" background="bg-red-400">{checked ? 'Ativo' : 'Inativo'}</SlideToggle>
-  </form>
-</div>
+      update()
+    }
+  }}
+  class="w-28"
+>
+  <input type="hidden" name="id" value={id} />
+  <SlideToggle {disabled} on:click={handleAlternar} {name} {checked} size="sm" active="bg-green-500" background="bg-red-400">
+    {#if checked}
+      Ativo &nbsp;&nbsp;
+    {:else}
+      Inativo
+    {/if}
+  </SlideToggle>
+</form>
