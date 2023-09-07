f0937caef7f626e80ef3cb8de324958ba2c57773
Glenn Fonseca <glenn.fonseca@outlook.com>
2023-09-07 10:29:19 -0400

    dirname

--- src/lib/pdf.cjs
+++ src/lib/pdf.cjs
@@ -1,7 +1,10 @@
 import PdfPrinter from 'pdfmake';
 import path from "node:path";
 import { cwd } from 'node:process';
+import { fileURLToPath } from 'node:url';
 
+var __filename = fileURLToPath(import.meta.url);
+var __dirname = path.dirname(__filename);
 const fonts = {
   Quicksand: {
     normal: path.join(cwd(), "/static/fonts/Quicksand.ttf"),
